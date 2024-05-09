4d37316f41d92bb5a36c9a4cd9cf44417bba3e8b
Tien Anh <99895433+tienanh109@users.noreply.github.com>
2024-05-09 10:34:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import discord
+import requests
+from discord.ext import commands
 
+API_KEY = "YOUR API KEY"
+
+bot = commands.Bot(command_prefix='/')
+
+@bot.slash_command(name="gemini-pro-ai", description="AI by Google!")
+async def ai(ctx, prompt: str):
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={API_KEY}"
+    headers = {'Content-Type': 'application/json'}
+    data = {"contents": [{"parts": [{"text": prompt}]}]}
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        content = response.json()["candidates"][0]["content"]["parts"][0]["text"]
+        await ctx.respond(content=content)
+    else:
+        await ctx.respond(content="Error!")
+
+bot.run("YOUR BOT TOKEN")
