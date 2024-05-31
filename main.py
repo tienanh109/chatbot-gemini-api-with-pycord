417445bdf539aaa8b83c63be0dbe76497f0f1b23
Tien Anh <99895433+tienanh109@users.noreply.github.com>
2024-05-31 20:29:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+REMEMBER, THE CODE IS COMPLETELY PROVIDED BY TIENANH109
+"""
 import discord
 import requests
 from discord.ext import commands
@@ -14,6 +17,7 @@ async def ai(ctx, prompt: str):
     response = requests.post(url, headers=headers, json=data)
     if response.status_code == 200:
         content = response.json()["candidates"][0]["content"]["parts"][0]["text"]
+        await ctx.defer()
         await ctx.respond(content=content)
     else:
         await ctx.respond(content="Error!")
